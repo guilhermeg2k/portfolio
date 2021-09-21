4a54a1b5a95456ee4588405b4bb9f3960d6736c1
guilhermeg2k <guilhermen2k@gmail.com>
2021-09-21 19:12:34 -0300

    change: changed animation direction of text on skills page

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -43,8 +43,8 @@ export default function Skills() {
               }
             </motion.div>
             <motion.div
-              initial={{ x: "1000" }}
-              animate={{ x: 0 }}
+              initial={{ y: "1000" }}
+              animate={{ y: 0 }}
               transition={{ duration: 0.5 }}
               className={styles.skillsText}
             >
